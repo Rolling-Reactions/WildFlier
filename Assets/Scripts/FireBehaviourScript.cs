8b7d02aeb4be4e4fb0a915cca398d58bcd727e2a
anna-sara-maria <ak9522@gmail.com>
2023-12-04 15:50:46 +0100

    Fire Spawner script

--- Assets/Scripts/FireBehaviourScript.cs
+++ Assets/Scripts/FireBehaviourScript.cs
@@ -1,75 +1,77 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//// td.GetTreeInstances
 
-public class FireBehaviourScript : MonoBehaviour
-{
-    public TerrainData terrainData;
-    private TreeInstance[] trees;
-    private bool[] isTreeOnFire;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        trees = terrainData.treeInstances;
-        isTreeOnFire = new bool[trees.Length];
+//public class FireBehaviourScript : MonoBehaviour
+//{
+//    public TerrainData terrainData;
+//    private TreeInstance[] trees;
+//    private bool[] isTreeOnFire;
 
-        // Choose a random tree to start the fire
-        int randomTree = Random.Range(0, trees.Length);
-        StartFire(randomTree);
-    }
+//    // Start is called before the first frame update
+//    void Start() 
+//    {
+//        trees = terrainData.treeInstances;
+//        isTreeOnFire = new bool[trees.Length];
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Check for spreading fire
-        for (int i = 0; i < trees.Length; i++)
-        {
-            if (isTreeOnFire[i])
-            {
-                // SpreadFire(i);
-            }
-        }
-    }
+//        // Choose a random tree to start the fire
+//        int randomTree = Random.Range(0, trees.Length);
+//        StartFire(randomTree);
+//    }
 
-    void StartFire(int treeIndex)
-    {
-        isTreeOnFire[treeIndex] = true;
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        // Check for spreading fire
+//        for (int i = 0; i < trees.Length; i++)
+//        {
+//            if (isTreeOnFire[i])
+//            {
+//                // SpreadFire(i);
+//            }
+//        }
+//    }
 
-        trees[treeIndex].color = Color.red; // You can change the tree color to represent it's on fire
-        StartCoroutine(SpreadFireAfterDelay(treeIndex, 10f)); // Spread fire after 10 seconds
-    }
+//    void StartFire(int treeIndex)
+//    {
+//        isTreeOnFire[treeIndex] = true;
 
-    /*
-    void SpreadFire(int sourceTreeIndex)
-    {
-        Vector3 sourcePosition = terrainData.GetTreeInstancePosition(sourceTreeIndex);
+//        trees[treeIndex].color = Color.red; // You can change the tree color to represent it's on fire
+//        StartCoroutine(SpreadFireAfterDelay(treeIndex, 10f)); // Spread fire after 10 seconds
+//    }
 
-        for (int i = 0; i < trees.Length; i++)
-        {
-            if (i != sourceTreeIndex && !isTreeOnFire[i])
-            {
-                Vector3 targetPosition = terrainData.GetTreeInstancePosition(i);
-                float distance = Vector3.Distance(sourcePosition, targetPosition);
+//    /*
+//    void SpreadFire(int sourceTreeIndex)
+//    {
+//        Vector3 sourcePosition = terrainData.GetTreeInstancePosition(sourceTreeIndex);
 
-                // Check if the tree is within the specified radius
-                if (distance <= 5f)
-                {
-                    // Probability of catching fire can be adjusted
-                    if (Random.Range(0f, 1f) < 0.1f)
-                    {
-                        StartFire(i);
-                    }
-                }
-            }
-        }
-    }*/
+//        for (int i = 0; i < trees.Length; i++)
+//        {
+//            if (i != sourceTreeIndex && !isTreeOnFire[i])
+//            {
+//                Vector3 targetPosition = terrainData.GetTreeInstancePosition(i);
+//                float distance = Vector3.Distance(sourcePosition, targetPosition);
 
-    IEnumerator SpreadFireAfterDelay(int treeIndex, float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        SpreadFire(treeIndex);
-    }
-}
+//                // Check if the tree is within the specified radius
+//                if (distance <= 5f)
+//                {
+//                    // Probability of catching fire can be adjusted
+//                    if (Random.Range(0f, 1f) < 0.1f)
+//                    {
+//                        StartFire(i);
+//                    }
+//                }
+//            }
+//        }
+//    }*/
 
-/**/
+//    IEnumerator SpreadFireAfterDelay(int treeIndex, float delay)
+//    {
+//        yield return new WaitForSeconds(delay);
+//        SpreadFire(treeIndex);
+//    }
+//}
+
+///**/
---
+++ Assets/Scripts/FireSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSpawner : MonoBehaviour
+{
+    TreeGrid tg;
+    public TerrainData terrainData;
+    private TreeInstance[] trees;
+    public GameObject firePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trees = terrainData.treeInstances;
+        tg = new TreeGrid(terrainData, 20.0f);
+
+        // Choose a random tree to start the fire
+        int randomTree = Random.Range(0, trees.Length);
+        GameObject fire = Instantiate(firePrefab);
+        fire.transform.position = Vector3.Scale(terrainData.GetTreeInstance(randomTree).position, terrainData.size);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
